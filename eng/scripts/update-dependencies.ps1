b32116eea3f39ab20aab7b442248df6277d091dd
Frank Buckley <frank@frankbuckley.com>
2023-06-25 09:33:02 +0100

    Create update-dependencies.ps1

---
+++ eng/scripts/update-dependencies.ps1
@@ -0,0 +1,32 @@
+## Copyright (c) Down Syndrome Education Enterprises CIC. All Rights Reserved.
+## Information contained herein is Proprietary and Confidential.
+
+[CmdletBinding(PositionalBinding = $false)]
+param (
+    [bool]$publicRelease = $false
+)
+
+Set-StrictMode -version 2.0
+$ErrorActionPreference = "Stop"
+
+function Update-Dependencies() {
+
+    . (Join-Path $PSScriptRoot "utils.ps1")
+
+    Write-Host
+    Write-Host "--------------------------------------------------------------------------------" -ForegroundColor Cyan
+    Write-Host "Update-Dependencies" -ForegroundColor Cyan
+    Write-Host "--------------------------------------------------------------------------------" -ForegroundColor Cyan
+    Write-Host
+
+    &dotnet-outdated -pre auto --upgrade .\DSE.Open.RazorToolkit.sln
+}
+
+try {
+    Update-Dependencies
+}
+catch {
+    Write-Host $_
+    Write-Host $_.Exception
+    exit 1
+}
