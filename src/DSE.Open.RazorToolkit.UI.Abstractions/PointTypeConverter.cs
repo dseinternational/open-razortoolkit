5d3226207a19cc349c15dfc04e178501fe64d106
Frank Buckley <frank@frankbuckley.com>
2023-07-05 13:24:03 +0100

    Move Point

--- src/DSE.Open.RazorToolkit.UI.Abstractions/PointTypeConverter.cs
+++ src/DSE.Open.RazorToolkit.UI.Abstractions/PointTypeConverter.cs
@@ -1,8 +1,9 @@
-﻿// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
 // Down Syndrome Education International and Contributors licence this file to you under the MIT license.
 
 using System.ComponentModel;
 using System.Globalization;
+using DSE.Open.Drawing;
 using DSE.Open.RazorToolkit.Core;
 
 namespace DSE.Open.RazorToolkit.UI.Abstractions;
