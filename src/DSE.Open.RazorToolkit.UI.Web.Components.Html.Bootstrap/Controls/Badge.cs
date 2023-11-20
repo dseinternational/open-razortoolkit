5d84c1d22ea0c001999cee964283fa9649fd3236
Frank Buckley <frank@frankbuckley.com>
2023-11-20 11:22:24 +0000

    Create Badge.cs

---
+++ src/DSE.Open.RazorToolkit.UI.Web.Components.Html.Bootstrap/Controls/Badge.cs
@@ -0,0 +1,27 @@
+// Copyright (c) Down Syndrome Education International and Contributors. All Rights Reserved.
+// Down Syndrome Education International and Contributors licence this file to you under the MIT license.
+
+using DSE.Open.RazorToolkit.UI.Abstractions;
+using Microsoft.AspNetCore.Components;
+
+namespace DSE.Open.RazorToolkit.UI.Web.Components.Html.Bootstrap.Controls;
+
+public class Badge : HtmlSpan
+{
+    [Parameter]
+    public bool RoundedPill { get; set; }
+
+    protected override void BuildClasses(ClassBuilder classBuilder)
+    {
+        Guard.IsNotNull(classBuilder);
+
+        classBuilder.Add(BootstrapClasses.Badge);
+
+        if (RoundedPill)
+        {
+            classBuilder.Add(BootstrapClasses.RoundedPill);
+        }
+
+        base.BuildClasses(classBuilder);
+    }
+}
